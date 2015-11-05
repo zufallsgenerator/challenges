c7e07ae7f81473b798804516b140a09af020c05b
Valérian Galliat <val@codejam.info>
2015-11-04 23:17:58 -0500

    Output formatting

--- haskell/c0ffee4bdb4630a97e514e35695913a6322defa9/Main.hs
+++ haskell/c0ffee4bdb4630a97e514e35695913a6322defa9/Main.hs
@@ -2,12 +2,13 @@ import Data.List (find)
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Base16 as Base16
 import qualified Crypto.Hash.SHA1 as SHA1
+import Text.Printf (printf)
 
 -- Format an email given a prefix, suffix, and variable part
 fmtEmail prefix suffix i = prefix ++ i ++ suffix
 
 -- Generate a list of all possible integers
-bruteIntegers = iterate (+ 1) 0
+bruteIntegers = [0..]
 
 -- Casted as strings
 bruteIntegerStrings = map show bruteIntegers
@@ -26,10 +27,21 @@ coffee = fst $ Base16.decode $ Char8.pack "c0ffee"
 -- Test if given bytes begins with coffee bytes
 isCoffee bytes = coffee == Char8.take (Char8.length coffee) bytes
 
+-- Hash a string
+sha1 str = SHA1.hash $ Char8.pack str
+
+-- Encoded version
+sha1hex str = Char8.unpack $ Base16.encode $ sha1 str
+
 -- Test if given hashed email begins with coffee
-isCoffeeEmail email =
-  isCoffee $ SHA1.hash $ Char8.pack email
+isCoffeeStr str = isCoffee $ sha1 str
+
+display :: (Integer, String) -> IO ()
+display (i, email) =
+  let hash = sha1hex email
+  in printf "Found %s with hash %s after %d tries\n" email hash i
 
 main =
-  let match = find isCoffeeEmail bruteMyEmails
-  in case match of Just match -> putStrLn match
+  let indexedEmails = zip [0..] bruteMyEmails
+      match = find (isCoffeeStr . snd) indexedEmails
+  in case match of Just match -> display match
